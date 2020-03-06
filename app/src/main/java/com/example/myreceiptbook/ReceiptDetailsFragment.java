12b5a7b64c5ab4a0acd3c4d44c64813c1c5b9167
apele <pele_adriana@yahoo.com>
2020-03-06 07:54:07 +0200

    populate xml file with views

--- app/src/main/java/com/example/myreceiptbook/ReceiptDetailsFragment.java
+++ app/src/main/java/com/example/myreceiptbook/ReceiptDetailsFragment.java
@@ -1,19 +1,28 @@
 package com.example.myreceiptbook;
 
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.squareup.picasso.Picasso;
+
 public class ReceiptDetailsFragment extends Fragment
 {
     private ReceiptDetailsViewModel receiptDetailsViewModel;
+
+    private ImageView imageView;
+    private TextView titleTextView;
+    private TextView longDescTextView;
 
     public static ReceiptDetailsFragment newInstance()
     {
@@ -23,7 +32,13 @@ public class ReceiptDetailsFragment extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.receipt_details_fragment, container, false);
+        final View rootView = inflater.inflate(R.layout.receipt_details_fragment, container, false);
+
+        imageView = rootView.findViewById(R.id.image);
+        titleTextView = rootView.findViewById(R.id.title);
+        longDescTextView = rootView.findViewById(R.id.longDesc);
+
+        return rootView;
     }
 
     @Override
@@ -34,12 +49,16 @@ public class ReceiptDetailsFragment extends Fragment
         this.getView().setBackgroundColor(Color.WHITE);
 
         receiptDetailsViewModel = ViewModelProviders.of(getActivity()).get(ReceiptDetailsViewModel.class);
-        receiptDetailsViewModel.getSelectedReceipt().observe(getViewLifecycleOwner(), item ->
+        receiptDetailsViewModel.getSelectedReceipt().observe(getViewLifecycleOwner(), receipt ->
         {
-            // show title
-            // show image
-            // show desc short
-            // show desc long
+            longDescTextView.setText(receipt.getLargeDescription());
+            titleTextView.setText(receipt.getTitle());
+
+            Picasso.with(getContext())
+                    .load(Uri.parse(receipt.getImageUri()) + ".png")
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_background)
+                    .into(imageView);
         });
     }
 
